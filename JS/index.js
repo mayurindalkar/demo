0a6441630988a0aead082928048711a6d5d299f8
MayurIndalkar <mayurindalkar161@gmail.com>
2022-09-18 23:01:17 +0530

    my first commit

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+console.time("yor code took");
+console.log("hiii mayur here")
+console.log(10+10)
+console.log(true)
+console.log([1,1,1,1,1])
+console.log({harry: 'dj',mayur:'rock'})
+console.table({harry: 'dj',mayur:'rock'})
+// console.warn("this is danger")
+// console.clear()
+// console.error("erorr")
+//console.timeEnd("yor code took");
+// console.assert(6<0, "this is not possible")
---
+++ JS/variable.js
@@ -0,0 +1,7 @@
+ /* varibles in js var,let,const    var='' "" ``*/
+ var name ="mayur"
+ var ch ="roock"
+var num=11+11
+
+ console.log(name,ch,num)
+ 
